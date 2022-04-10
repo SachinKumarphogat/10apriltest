b978013f47dd90d490b29a98d048b5789a3ad14c
SachinKumarphogat <100225961+SachinKumarphogat@users.noreply.github.com>
2022-04-10 18:48:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const eventHandler = function(){
+	const input =document.getElementById("input").value;
+alert(input);
+}
+
+const btn=document.getElementById("btn")
+btn.addEventListener('click',eventHandler);
